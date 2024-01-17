0d620c44687423c9e25d1cee25877b602bad03cc
regina gimutao <gimutaoregina191@gmail.com>
2024-01-17 09:33:13 +0800

    trello-clone

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button"
+
+export const Footer = () => {
+
+    const year = new Date().getFullYear();
+
+    return (
+        <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
+            <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+            <p className="flex flex-col items-start justify-between">Taskify &copy; {year}</p>
+                <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+                    <Button size="sm" variant="ghost">
+                            Privacy Policy
+                    </Button>
+                    <Button size="sm" variant="ghost">
+                            Terms of Service
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
